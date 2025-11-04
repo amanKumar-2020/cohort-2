910d0c7b5365c88f2d8a479267e508af06425d33
Aman kumar <jnvaman4@gmail.com>
2025-11-04 10:42:11 +0530

    solve day-34 problem no. 13

--- Assigment/Day34/script.js
+++ Assigment/Day34/script.js
@@ -1,17 +1,26 @@
 // 12. Ask user for words until they type “stop”. Count how many times they typed “yes”
 // Loop until "stop" is typed. Count "yes".
-let yesCount = 0;
 
-while (true) {
-  let input = prompt("Enter your word");
-  if (input === null) {
-    continue;
-  }
-  if (input === "yes") {
-    yesCount++;
-  }
-  if (input.toLowerCase() === "stop") {
-    break;
+// let yesCount = 0;
+// while (true) {
+//   let input = prompt("Enter your word");
+//   if (input === null) {
+//     continue;
+//   }
+//   if (input === "yes") {
+//     yesCount++;
+//   }
+//   if (input.toLowerCase() === "stop") {
+//     break;
+//   }
+// }
+// console.log(yesCount);
+
+// 13. Print numbers divisible by 7 from 1 to 50
+// Use modulo % and loop.
+
+for (let i = 1; i < 50; i++) {
+  if (i % 7 === 0) {
+    console.log(`${i} is divisible by 7`);
   }
 }
-console.log(yesCount);
