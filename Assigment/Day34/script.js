8bb32ac1956e4cb1d2ea3b5906133270a278e034
Aman kumar <jnvaman4@gmail.com>
2025-11-04 10:47:24 +0530

    solve day-34 problem no. 14

--- Assigment/Day34/script.js
+++ Assigment/Day34/script.js
@@ -16,11 +16,25 @@
 // }
 // console.log(yesCount);
 
+// --------------XXX------------------XXX------------------------------XXX---------------------
+
 // 13. Print numbers divisible by 7 from 1 to 50
 // Use modulo % and loop.
 
-for (let i = 1; i < 50; i++) {
-  if (i % 7 === 0) {
-    console.log(`${i} is divisible by 7`);
+// for (let i = 1; i < 50; i++) {
+//   if (i % 7 === 0) {
+//     console.log(`${i} is divisible by 7`);
+//   }
+// }
+
+// --------------XXX------------------XXX------------------------------XXX---------------------
+
+// 14. Sum of all odd numbers from 1 to 30
+// Add only odd numbers. Print final sum.
+let oddNumberSum = 0;
+for (let i = 0; i < 30; i++) {
+  if (i % 2 !== 0) {
+    oddNumberSum += i;
   }
 }
+console.log(`sum of all odd Number is ${oddNumberSum}`);
