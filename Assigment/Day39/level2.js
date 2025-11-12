ac04799c560ba3b1c07cece2261856bbb8d609cc
Aman kumar <jnvaman4@gmail.com>
2025-11-12 10:28:45 +0530

    Day 39 level-2 ques-02

--- Assigment/Day39/level2.js
+++ Assigment/Day39/level2.js
@@ -1,14 +1,37 @@
 // Level 2 – Functional Thinking & Logic Tasks(Intermediate)
 
 // 1. Write a higher-order function `runTwice(fn)` that takes another function and executes it two times.
-function runTwice(fn) {
-  console.log("first line of hod function hai");
-  fn()
-  fn()
+
+// function runTwice(fn) {
+//   console.log("first line of hod function hai");
+//   fn()
+//   fn()
+// }
+
+// let paramFn = function(){
+//   console.log("I am in second funtion");
+
+// }
+// runTwice(paramFn);
+
+// -----------------------------XXXXXXX----------------------XXXXXX--------------------
+
+// 2. Create one pure function that always returns thesame output for a given input, and one impurefunction using a global variable.
+
+function pureFn(a, b) {
+  return a + b;
+}
+let sum = pureFn(12, 3);
+
+let globalVariable = 3;
+
+function impureFn(num) {
+  let b = Math.floor((Math.random())*10);
+  // globalVariable++;  //it up to you
+  return b + num;
 }
 
-let paramFn = function(){
-  console.log("I am in second funtion");
-  
-}
-runTwice(paramFn);
+let ran = impureFn(globalVariable);
+
+console.log(` it is pure funtion ${sum}`);
+console.log(` it is Impure funtion ${ran}`);
