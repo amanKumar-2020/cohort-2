def7f48a8db72eb8f59077f52784764ccf4dc659
Aman kumar <jnvaman4@gmail.com>
2025-11-12 10:48:03 +0530

    Day 39 level-2 ques-03

--- Assigment/Day39/level2.js
+++ Assigment/Day39/level2.js
@@ -18,20 +18,38 @@
 
 // 2. Create one pure function that always returns thesame output for a given input, and one impurefunction using a global variable.
 
-function pureFn(a, b) {
-  return a + b;
+// function pureFn(a, b) {
+//   return a + b;
+// }
+// let sum = pureFn(12, 3);
+
+// let globalVariable = 3;
+
+// function impureFn(num) {
+//   let b = Math.floor(Math.random() * 10);
+//   // globalVariable++;  //it up to you
+//   return b + num;
+// }
+
+// let ran = impureFn(globalVariable);
+
+// console.log(` it is pure funtion ${sum}`);
+// console.log(` it is Impure funtion ${ran}`);
+
+// -----------------------------XXXXXXX----------------------XXXXXX--------------------
+
+// 3. Write a function that uses object destructuringinside parameters to extract and print `name` and`age`
+
+const user1 = {
+  name: "Aman",
+  age: 69,
+  weight: 101,
+};
+
+function displayUser({ name, age }) {
+  // const { name, age } = user;  // destructuringinside inside the body
+  console.log(`name of user is ${name}`);
+  console.log(`name of age is ${age}`);
 }
-let sum = pureFn(12, 3);
 
-let globalVariable = 3;
-
-function impureFn(num) {
-  let b = Math.floor((Math.random())*10);
-  // globalVariable++;  //it up to you
-  return b + num;
-}
-
-let ran = impureFn(globalVariable);
-
-console.log(` it is pure funtion ${sum}`);
-console.log(` it is Impure funtion ${ran}`);
+displayUser(user1);
