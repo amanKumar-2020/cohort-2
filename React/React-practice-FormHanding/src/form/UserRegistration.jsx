97ef014018dedb3b93fe611c06dd1aee45d75195
Aman kumar <jnvaman4@gmail.com>
2026-01-25 03:17:41 +0530

    form handing handlechange

--- React/React-practice-FormHanding/src/form/UserRegistration.jsx
+++ React/React-practice-FormHanding/src/form/UserRegistration.jsx
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import "./UserRegistration.css";
 
 const UserRegistration = () => {
-  // const [username, setUsername] = useState("");
-  // const [email,setEmail] = useState("");
-  // const [password, setPassword] = useState("")
   const [formData, setFormData] = useState({
     username : "",
     email : "",
@@ -16,43 +13,56 @@ const UserRegistration = () => {
     console.log(formData.username);
     console.log(formData.email);
     // console.log(e.target.value);
-    
-    
   };
-
+  const handleChange = function(e){
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+    })
+  }
 
   return (
     <div className="container">
       <form className="form" onSubmit={handleSubmit}>
         <input
           className="input"
+          name="username"
           type="text"
           value={formData.username}
-          onChange={(e) => setFormData({
-            ...formData,
-            username: e.target.value
-          })}
+          // onChange={(e) => setFormData({
+          //   ...formData,
+          //   username: e.target.value
+          // })}
+          onChange={handleChange}
           placeholder="Enter your Name"
         />
-        <input 
-        type="email" 
-        value={formData.email}
-        placeholder="Enter your Email"
-        onChange={(e)=> setFormData({
-          ...formData,
-          email: e.target.value
-        })}
-        className="input email_input"
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          placeholder="Enter your Email"
+          // onChange={(e) =>
+          //   setFormData({
+          //     ...formData,
+          //     email: e.target.value,
+          //   })
+          // }
+          onChange={handleChange}
+          className="input email_input"
         />
-        <input 
-        type="password"
-        value={formData.password}
-        onChange={(e)=>setFormData({
-          ...formData,
-          password: e.target.value
-        })}
-        className="input"
-        placeholder="Enter your password"
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          // onChange={(e) =>
+          //   setFormData({
+          //     ...formData,
+          //     password: e.target.value,
+          //   })
+          // }
+          onChange={handleChange}
+          className="input"
+          placeholder="Enter your password"
         />
         <button className="button" type="submit">
           Submit
