93d1122b0f9a4d4024b0a3f6d9e98da42f6c6ca0
Aman kumar <jnvaman4@gmail.com>
2026-02-11 11:47:32 +0530

    Day-13 Backend practice

---
+++ Backend/Day-13/src/config/database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+
+const connectToDb = function(){
+
+}
+module.exports = connectToDb;
---
+++ Backend/Day-13/src/routes/note.routes.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const noteModel = require("../models/note.model")
+
+const noteRouter = express.Router();
+
+
+module.exports = noteRouter; 
