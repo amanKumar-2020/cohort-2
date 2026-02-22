1a171b7cbfd9e5700f3b0b3526aa293156bec511
Aman kumar <jnvaman4@gmail.com>
2026-02-22 22:06:41 +0530

    improve code formatting and consistency in post controller

--- Backend/Day-15/src/controllers/post.controller.js
+++ Backend/Day-15/src/controllers/post.controller.js
@@ -1,9 +1,8 @@
 const postModel = require("../models/post.model");
-const { toFile } = require("@imagekit/nodejs") ;
-const ImageKit = require("@imagekit/nodejs"); 
-const jwt = require("jsonwebtoken")
+const { toFile } = require("@imagekit/nodejs");
+const ImageKit = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
 
 const client = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
@@ -11,43 +10,60 @@ const client = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-async function createPostController(req,res) {
-  console.log(req.body,req.file);
+async function createPostController(req, res) {
+  console.log(req.body, req.file);
 
   if (!req.file) {
     return res.status(400).json({ message: "Image required" });
   }
   const token = req.cookies.token;
-  if(!token){
+  if (!token) {
     return res.status(401).json({
-      message: "Token not provided , Unauthorized access"
+      message: "Token not provided , Unauthorized access",
     });
   }
   let decoded = null;
   try {
     decoded = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (error){
+  } catch (error) {
     return res.status(401).json({
-      message : "user not authorized"
-    })
+      message: "user not authorized",
+    });
   }
-  console.log(decoded);
+  // console.log(decoded);
 
   const uploadedFile = await client.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "Test",
     folder: "cohort-2-insta-clone-posts",
   });
-  
+
   const post = await postModel.create({
-    caption : req.body.caption ,
+    caption: req.body.caption,
     imgUrl: uploadedFile.url,
-    user  : decoded.id
-  })
+    user: decoded.id,
+  });
   res.status(201).json({
-    message : "post created successful",
-    post
-  })
+    message: "post created successful",
+    post,
+  });
 }
 
-module.exports = {createPostController}
+async function getPostController(req, res) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      message: "Token not provided , Unauthorized access",
+    });
+  }
+  let decoded = null;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "user not authorized",
+    });
+  }
+}
+
+module.exports = { createPostController };
