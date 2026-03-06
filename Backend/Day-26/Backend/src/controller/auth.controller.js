e2a024f46240e4384fba8524648d0a37fec854b3
Aman kumar <jnvaman4@gmail.com>
2026-03-06 20:04:19 +0530

    feat: add login functionality with validation and JWT token generation

--- Backend/Day-26/Backend/src/controller/auth.controller.js
+++ Backend/Day-26/Backend/src/controller/auth.controller.js
@@ -7,7 +7,7 @@ require("dotenv").config()
 async function registerController(req,res) {
     const {email , password , username} = req.body;
 
-    if((!email || !username) && !password){
+    if(!email && !username && !password){
         return res.status(400).json({
             message:"All field required"
         })
@@ -45,4 +45,44 @@ async function registerController(req,res) {
     })
 }
 
-module.exports = {registerController};
+async function loginController(req,res) {
+    const {email , username ,password} = req.body;
+
+    if ((!email || !username) && !password) {
+      return res.status(400).json({
+        message: "All field required",
+      });
+    }
+    const user = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(!user){
+        return res.status(401).json({
+            message:"user not found"
+        })
+    }
+    const isPasswordValid = bcryptjs.compare(user.password == password);
+    if(!isPasswordValid){
+        return res.status(400).json({
+          message: "Invalid credentials",
+        });
+    }
+    const token = jwt.sign(
+        {id:user._id , username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn:"3d"}
+    )
+    res.cookie("token", token)
+    res.status(200).json({
+        message:"user login successful",
+        user:{
+            username:user.username,
+            email: user.email
+        }
+    })
+}
+
+module.exports = {registerController ,loginController};
--- Backend/Day-26/Backend/src/routes/auth.route.js
+++ Backend/Day-26/Backend/src/routes/auth.route.js
@@ -4,5 +4,6 @@ const authController =require("../controller/auth.controller")
 const appRouter =express.Router()
 
 appRouter.post("/register",authController.registerController)
+appRouter.post("/login",authController.loginController)
 
 module.exports = appRouter;
